837722d83136de75eedbc711c5746a4ee829b4c9
jmdorso <juanmadorso@gmail.com>
2020-08-27 00:40:01 -0300

    Prueba

---
+++ Ejercicios/Ejercicio 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 02";
+            int numero = 0;
+            Console.WriteLine("INGRESE NUMERO");
+            string cadena = Console.ReadLine();
+            int.TryParse(cadena, out numero);//para convertir
+            while(numero <= 0)
+            {
+                Console.WriteLine("ERROR. REINGRESE NUMERO");
+                cadena = Console.ReadLine();
+                int.TryParse(cadena, out numero);//para convertir
+            }
+            Console.WriteLine("El cuadrado es {0} y el cubo es {1}", Math.Pow(numero, 2), Math.Pow(numero, 3));
+            Console.ReadKey();
+                    
+
+        }
+    }
+}
---
+++ Ejercicios/Ejercicios/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 01";
+            int numero = 0;
+            int acum = 0;
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            double promedio;
+            int i;
+
+            for(i=0;i<5;i++)
+            {
+                Console.WriteLine("INGRESE NUMERO");
+                string cadena = Console.ReadLine();
+                int.TryParse(cadena, out numero);//para convertir
+                acum = numero + acum;
+                if (max < numero) 
+                {
+                    max = numero;
+                }
+                if(min > numero)
+                {
+                    min = numero;
+                }
+            }
+            Console.WriteLine("El Numero Maximo es: {0}", max);
+            Console.WriteLine("El Numero Minimo es: {0}", min);
+            promedio = (double)acum / i;
+            Console.WriteLine("El promedio es: {0:#,###.00}", promedio);
+            Console.ReadKey();
+
+        }
+    }
+}
