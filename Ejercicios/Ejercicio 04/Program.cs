c954f618115ffd0eb80704131b67f5b3238e2b30
jmdorso <juanmadorso@gmail.com>
2020-08-27 13:01:19 -0300

    Ejercicio 4

---
+++ Ejercicios/Ejercicio 04/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 04";
+            int numeroActual, numeroQueDivide;
+            int contadorDeNumPerfectos = 0;
+            int acumDivisores = 0;
+
+            for(numeroActual=1; contadorDeNumPerfectos < 4;numeroActual++)
+            {
+                acumDivisores = 0;
+                for(numeroQueDivide=1; numeroQueDivide<numeroActual;numeroQueDivide++)
+                {
+                    if (numeroActual % numeroQueDivide == 0)
+                    {
+                        acumDivisores += numeroQueDivide;
+                    }
+                }
+                if (acumDivisores == numeroActual)
+                {
+                    contadorDeNumPerfectos++;
+                    Console.WriteLine("El numero {0} es un numero perfecto, ya que la suma de sus divisores da {1}", numeroActual, acumDivisores);
+                }
+            }
+            Console.ReadKey();
+        }
+            
+    }
+}
--- Ejercicios/Ejercicio 3/Program.cs
+++ Ejercicios/Ejercicio 3/Program.cs
@@ -11,44 +11,43 @@ namespace Ejercicio_3
         static void Main(string[] args)
         {
             Console.Title = "Ejercicio 03";
-            int numeroADividir, numeroQueDivide;
+            int numeroActual, numeroQueDivide;
             int contadorDeNumPrimos = 0;
-            int numero = 0;
+            int numeroUsuario = 0;
             int resto = 0;
 
             Console.Write("INGRESE UN NUMERO Y LE MOSTRAREMOS TODOS LOS NUMEROS PRIMOS HASTA SU ELECCION: ");
             string cadena = Console.ReadLine();
-            int.TryParse(cadena, out numero);//para convertir
-            if (numero < 0)
+            int.TryParse(cadena, out numeroUsuario);//para convertir
+            if (numeroUsuario < 0)
             {
                 Console.WriteLine("\nLos numoros negativos no son primos");
             }
-            else if(numero < 2)
+            else if(numeroUsuario < 2)
             {
-                Console.WriteLine("\nNo existen numeros primos antes que el {0}",numero);
+                Console.WriteLine("\nNo existen numeros primos antes que el {0}",numeroUsuario);
             }
             else
             {
-                Console.WriteLine("\nLista de numeros primos hasta el {0}\n", numero);
-                for (numeroADividir = 2; numeroADividir <= numero; numeroADividir++)
+                Console.WriteLine("\nLista de numeros primos hasta el {0}\n", numeroUsuario);
+                for (numeroActual = 2; numeroActual <= numeroUsuario; numeroActual++)
                 {
-                    for (numeroQueDivide = 1; numeroQueDivide <= numeroADividir; numeroQueDivide++)
+                    for (numeroQueDivide = 1; numeroQueDivide <= numeroActual; numeroQueDivide++)
                     {
-                        if (numeroADividir % numeroQueDivide == 0)
+                        if (numeroActual % numeroQueDivide == 0)
                         {
                             resto++;
-                        }
-                      
+                        } 
                     }
                     if(resto == 2)
                     {
-                        Console.WriteLine("#{0}", numeroADividir);
+                        Console.WriteLine("#{0}", numeroActual);
                         contadorDeNumPrimos++;
                         resto = 0;
                     }
                     resto = 0;
                 }
-                Console.WriteLine("\n\tHay {0} numeros primos hasta el # {1}", contadorDeNumPrimos, numero);
+                Console.WriteLine("\n\tHay {0} numeros primos hasta el # {1}", contadorDeNumPrimos, numeroUsuario);
             }
             Console.ReadKey();
         }
