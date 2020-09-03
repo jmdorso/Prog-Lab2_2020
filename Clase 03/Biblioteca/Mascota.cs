a5072696af65deb7c1b03922f5adb2630d63d64a
jmdorso <juanmadorso@gmail.com>
2020-09-02 22:09:52 -0300

    Clase 03

---
+++ Clase 03/Biblioteca/Mascota.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Mascota
+    {
+        private string nombre;
+        private DateTime fechaNacimiento;
+        private string especie;
+        
+        public Mascota(string nombre, DateTime fechaNacimiento, string especie)
+        {
+            this.nombre = nombre;
+            this.fechaNacimiento = fechaNacimiento;
+            this.especie = especie;
+        }
+
+        public string MostrarMascota()
+        {
+            StringBuilder stringBuilder = new StringBuilder();//UNA FORMA DE CREAR TEXTO ORGANIZADO;
+            stringBuilder.AppendLine($"Nombre: {this.nombre}");
+            stringBuilder.AppendLine($"Fecha de nacimiento: {this.fechaNacimiento.ToShortDateString()}");
+            stringBuilder.AppendLine($"Especie: {this.especie}");
+
+            return stringBuilder.ToString();
+        }
+
+        public int CalcularEdad()
+        {
+            DateTime hoy = DateTime.Today;
+
+            int edad = hoy.Year - this.fechaNacimiento.Year;
+
+            return edad;
+        }
+
+    }
+}
---
+++ Clase 03/Clase 03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioteca;
+
+namespace Clase_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mascota mascota1 = new Mascota("Salem", new DateTime(1990, 01, 20),"gato");
+            Mascota mascota2 = new Mascota("Fatiga", new DateTime(2000, 02, 18), "perro");
+            Mascota mascota3 = new Mascota("Perry", new DateTime(2006, 03, 05), "ornitorrinco");
+            Mascota mascota4 = new Mascota("Golden", new DateTime(1994, 04, 26), "perro");
+
+            Console.WriteLine(mascota1.MostrarMascota());
+            Console.WriteLine(mascota2.MostrarMascota());
+            Console.WriteLine(mascota3.MostrarMascota());
+            Console.WriteLine(mascota4.MostrarMascota());
+
+            Console.WriteLine($"{mascota1} tiene {mascota1.CalcularEdad()} años");//CREAR SET Y GET
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
